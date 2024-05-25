3dd1469ad1452b83b4496bc7528346fe66408baa
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-05-26 05:26:58 +0530

    Convert to LowerCase

--- String/04.cpp
+++ String/04.cpp
@@ -14,3 +14,23 @@ int main() {
     cout << "Lowercase: " << lowerCh << endl;
     return 0;
 }
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+char toLowerCase(char ch){
+    if(ch>='a'&& ch<='z') return ch;
+    else{
+        return  ch+32;
+    }
+}
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    char lowerCh = toLowerCase(ch);
+    cout << "Lowercase: " << lowerCh << endl;
+    return 0;
+}
