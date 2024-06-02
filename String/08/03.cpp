390c22f86e4b5ed8e56697ea1a47f675d353f80c
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-06-02 22:21:57 +0530

    Uncommon Words from Two Sentences(884)

---
+++ String/08/03.cpp
@@ -0,0 +1,39 @@
+/*
+A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+
+A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+Output: ["sweet","sour"]
+Example 2:
+
+Input: s1 = "apple apple", s2 = "banana"
+Output: ["banana"]
+*/
+
+  class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2){
+        unordered_map<string,int>mp;
+        stringstream ss1(s1),ss2(s2);
+        string word1,word2;
+
+        while(ss1>>word1){
+            mp[word1]++;
+        }
+          while(ss2>>word2){
+            mp[word2]++;
+        }
+        vector<string>ans;
+        for(auto it :mp){
+            if(it.second==1) ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
