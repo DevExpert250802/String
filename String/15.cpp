4c87162bea2f8d67770273c46001d2218dfda851
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-06-26 10:29:12 +0530

    Integer to Roman(12)

---
+++ String/15.cpp
@@ -0,0 +1,24 @@
+/*
+    Company Tags                : Amazon, Facebook, Microsoft, Twitter, Apple, Google, Paypal, Bloomberg, Adobe, InfoEdge
+    Leetcode Link               : https://leetcode.com/problems/integer-to-roman/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/convert-to-roman-no/1
+    
+*/
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int>val{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string>Symbol{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        string result;
+        for(int i=0;i<13;i++){
+            if(num==0)break;
+             int times=num/val[i];
+            while(times--){
+                result+=Symbol[i];
+            }
+            num=num%val[i];
+        }
+        return result;
+    }
+};
