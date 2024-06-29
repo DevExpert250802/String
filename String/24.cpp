0944888660fbf5ad53dfe935565c81f348bf5296
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-06-29 08:41:48 +0530

    Concatenated Words( 472 )

---
+++ String/24.cpp
@@ -0,0 +1,90 @@
+/*
+      Company Tags                : META
+      Leetcode Link               : https://leetcode.com/problems/concatenated-words/
+*/
+
+//Using DFS and Memoization - O(N*L^3)
+
+
+
+
+class Solution {
+public:
+     bool isConcat(string word,unordered_set<string>&st){
+        int l = word.length();
+        for(int i=0;i<l;i++){
+           string prefix= word.substr(0,i+1);
+           string suffix= word.substr(i+1);
+                
+            if((st.find(prefix) != st.end() && isConcat(suffix, st)) ||
+               (st.find(prefix) != st.end() && st.find(suffix) != st.end()))
+              return true;
+        }
+        return false;
+     }
+
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        int n = words.size();
+        vector<string> result;
+        unordered_set<string> st(begin(words), end(words));
+        
+        for(int i = 0; i<n; i++) {
+            if(isConcat(words[i], st)){
+                result.push_back(words[i]);
+            }
+        }
+        return result;
+    }
+};
+
+
+---------------------------------------------------------------------------------optimization------------------------------------------------------------------------------------------------------------------------------
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+
+
+class Solution {
+public:
+
+    unordered_map<string, bool> mp;
+    bool isConcat(string word, unordered_set<string>& st) {
+        if(mp.find(word) != mp.end())return mp[word];
+        int l = word.length();
+        
+        for(int i = 0; i<l; i++) {
+            string prefix  = word.substr(0, i+1);
+            string suffix  = word.substr(i+1);
+            
+            if((st.find(prefix) != st.end() && isConcat(suffix, st)) ||
+               (st.find(prefix) != st.end() && st.find(suffix) != st.end()))
+                return mp[word] = true;
+        }
+        
+        return mp[word] = false;
+        
+    }
+    
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        int n = words.size();
+        mp.clear();
+        vector<string> result;
+        unordered_set<string> st(begin(words), end(words));
+        
+        for(int i = 0; i<n; i++) {
+            if(isConcat(words[i], st)){
+                result.push_back(words[i]);
+            }
+        }
+        return result;
+    }
+}; 
+
+
+
