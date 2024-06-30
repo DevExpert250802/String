81b8de3aeff9783a56c066948a4d83647048eb65
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-06-30 12:14:56 +0530

    Find the Index of the First Occurrence in a String ( 28 )

--- String/25.cpp
+++ String/25.cpp
@@ -23,7 +23,7 @@ public:
 };
 
 
-//   T.C. : O(n∗m)   S.C. : O(1)
+//   T.C. : O(n∗m)     S.C. : O(1)
 
 
 //Approach 2 :  Updated naive Approach
@@ -45,7 +45,7 @@ public:
     }
 };
 
-// T.C. : O((n-m)∗m) S.C. : O(1)
+// T.C. : O((n-m)∗m)         S.C. : O(1)
 
 
 //Approach 3: Slicing approach
@@ -62,5 +62,70 @@ public:
     }
 };
 
-// T.C. : O((n-m)∗m) S.C. : O(1)
+// T.C. : O((n-m)∗m)     S.C. : O(1)
 
+
+
+
+
+//Approach 3: KMP approach
+
+class Solution {
+public:
+    void fillLPS(string &str, vector<int>& lps) {
+        int n = str.size();
+        int len = 0, i = 1;
+        lps[0] = 0; 
+        
+        while (i < n) {
+            if (str[i] == str[len]) {
+                len++;
+                lps[i] = len;
+                i++; 
+            } else {
+                if (len == 0) {
+                    lps[i] = 0;
+                    i++; 
+                } else {
+                    len = lps[len - 1];
+                }
+            }
+        }
+    }
+
+    int KMP(string Text, string Pattern) {
+        int n = Text.size(), m = Pattern.size();
+        vector<int> lps(m);
+        fillLPS(Pattern, lps);
+        
+        int i = 0, j = 0;
+        while (i < n) {
+            if (Text[i] == Pattern[j]) {
+                i++;
+                j++;
+            }
+
+            if (j == m) {
+                return i - j; 
+            } else if (i < n && Text[i] != Pattern[j]) {
+                if (j == 0) {
+                    i++;
+                } else {
+                    j = lps[j - 1];
+                }
+            }
+        }
+        return -1;  // Pattern not found
+    }
+
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) {
+            return 0; 
+        }
+        return KMP(haystack, needle);
+    }
+};
+
+
+
+// T.C. : O(n+m)     S.C. : O(m)
